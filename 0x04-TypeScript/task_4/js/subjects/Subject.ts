af6f16048b53ffa6faacdbcf7df42c412f1b06e6
Wanjohi Wanjiru <WanjohiWanjiru@gmail.com>
2025-09-15 16:14:26 +0300

    task_4

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,5 +1,11 @@
 // task_4/js/subjects/Subject.ts
 namespace Subjects {
+  // Make sure Teacher interface exists before using it
+  export interface Teacher {
+    firstName: string;
+    lastName: string;
+  }
+
   // Interface defining the Subject
   export interface Subject {
     teacher?: Teacher;
