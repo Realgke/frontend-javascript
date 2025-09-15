e28cf79726bce227172233472fa235b536bd4e88
Wanjohi Wanjiru <WanjohiWanjiru@gmail.com>
2025-09-15 16:13:01 +0300

    task_4

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,7 +1,17 @@
 // task_4/js/subjects/Subject.ts
 namespace Subjects {
+  // Interface defining the Subject
   export interface Subject {
-    teacher?: Teacher; // optional attribute
+    teacher?: Teacher;
     setTeacher(teacher: Teacher): void;
+  }
+
+  // Class implementing the Subject interface
+  export class SubjectClass implements Subject {
+    teacher?: Teacher;
+
+    setTeacher(teacher: Teacher): void {
+      this.teacher = teacher;
+    }
   }
 }
